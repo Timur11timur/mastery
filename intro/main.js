5eeb379985297f57805d29f7151485bb810b7ce6
Timur <timur__timur@mail.ru>
2021-02-01 01:33:49 +0200

    Add images

---
+++ intro/main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'Socks',
+		image: 'images/socks.jpg',
+		inventory: 10,
+		details: ['10% coton', '80% polyester'],
+		variants: [
+			{
+				variantId: 124,
+				variantColor: "green",
+				variantImg: "images/socks.jpg"
+			},
+			{
+				variantId: 324,
+				variantColor: "blue",
+				variantImg: "images/socks2.jpg"
+			}
+		],
+		cart: 0
+	},
+	methods: {
+		addToCart: function() {
+			this.cart++;
+		},
+		updateProduct(newImage) { 
+			this.image = newImage
+		}
+	}
+});
