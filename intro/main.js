034c9404477928e33ea094523d96b67651c75115
Timur <t.fralik@aimprosoft.com>
2021-02-01 18:59:42 +0200

    Add move to component

--- intro/main.js
+++ intro/main.js
@@ -1,31 +1,74 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		brand: 'Brand',
-		selectedVariant: 0,
-		product: 'Socks',
-		details: ['10% cotton', '80% polyester'],
-		variants: [
-			{
-				variantId: 124,
-				variantColor: "gray",
-				variantImg: "images/socks.jpg",
-				variantQuantity: 11
-			},
-			{
-				variantId: 254,
-				variantColor: "orange",
-				variantImg: "images/socks2.jpg",
-				variantQuantity: 5
-			},
-			{
-				variantId: 324,
-				variantColor: "red",
-				variantImg: "images/socks3.jpg",
-				variantQuantity: 0
-			}
-		],
-		cart: 0
+Vue.component('product', {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true
+		}
+	},
+	template: `
+		<div class="product flex">
+			<div class="product-image w-1/3 mx-4 my-2 border">
+				<img v-bind:src="image" class="object-contain h-50 w-full" alt="Image">
+			</div>inventory
+
+			<div class="product-info mx-2 my-2">
+				<h1 :class="{ 'text-blue-500': onSale }" class="text-4xl" v-text="title"></h1>
+				<p v-if="inStock > 10">In stock</p>
+				<p v-else-if="inStock <=10 && inStock > 0">Almost out of stock</p>
+				<p v-else>Out stock</p>
+				<p>Shipping: {{ shipping }}</p>
+				<ul class="list-disc ml-8">
+					<li v-for="detail in details">{{ detail }}</li>
+				</ul>
+				<div v-for="(variant, index) in variants"
+				:key="variant.variantId"
+				 class="mt-1 w-10 h-10"
+				:style="{ backgroundColor: variant.variantColor }"
+				@mouseover="updateProduct(index)">
+			</div>
+	
+			<div class="flex">
+				<button v-on:click="addToCart"
+				:disable="!inStock"
+				:class="{
+											'bg-grey-600': !inStock,
+											'bg-blue-600 text-white': inStock,
+										 }"
+				 class="hover:bg-blue-500 border rounded px-2 py-2 my-2">Add to Cart</button>
+				<div class="border mx-2 my-2">
+					<p class="px-2 py-2">Cart {{ cart }}</p>
+				</div>
+			</div>
+		</div>
+	</div>`,
+	data() {
+		return {
+			brand: 'Brand',
+			selectedVariant: 0,
+			product: 'Socks',
+			details: ['10% cotton', '80% polyester'],
+			variants: [
+				{
+					variantId: 124,
+					variantColor: "gray",
+					variantImg: "images/socks.jpg",
+					variantQuantity: 11
+				},
+				{
+					variantId: 254,
+					variantColor: "orange",
+					variantImg: "images/socks2.jpg",
+					variantQuantity: 5
+				},
+				{
+					variantId: 324,
+					variantColor: "red",
+					variantImg: "images/socks3.jpg",
+					variantQuantity: 0
+				}
+			],
+			cart: 0
+		}
 	},
 	methods: {
 		addToCart: function() {
@@ -48,6 +91,20 @@ var app = new Vue({
 		title() {
 			let sale = this.onSale ? ' (sale)' : '';
 			return this.brand + ' ' + this.product + sale
+		},
+		shipping() {
+			if (this.premium) {
+				return "Free"
+			}
+			return 2.99
 		}
+	}
+})
+
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		premium: true
 	}
 });
