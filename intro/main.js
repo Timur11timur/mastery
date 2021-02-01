dbba5ead09dc57458167986861506b19cb7e51d8
Timur <t.fralik@aimprosoft.com>
2021-02-01 17:53:55 +0200

    Add variety

--- intro/main.js
+++ intro/main.js
@@ -2,21 +2,27 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		brand: 'Brand',
+		selectedVariant: 0,
 		product: 'Socks',
-		image: 'images/socks.jpg',
-		inventory: 10,
-		details: ['10% coton', '80% polyester'],
-		inStock: true,
+		details: ['10% cotton', '80% polyester'],
 		variants: [
 			{
 				variantId: 124,
-				variantColor: "green",
-				variantImg: "images/socks.jpg"
+				variantColor: "gray",
+				variantImg: "images/socks.jpg",
+				variantQuantity: 11
+			},
+			{
+				variantId: 254,
+				variantColor: "orange",
+				variantImg: "images/socks2.jpg",
+				variantQuantity: 5
 			},
 			{
 				variantId: 324,
-				variantColor: "blue",
-				variantImg: "images/socks2.jpg"
+				variantColor: "red",
+				variantImg: "images/socks3.jpg",
+				variantQuantity: 0
 			}
 		],
 		cart: 0
@@ -25,13 +31,23 @@ var app = new Vue({
 		addToCart: function() {
 			this.cart++;
 		},
-		updateProduct(newImage) { 
-			this.image = newImage
+		updateProduct(index) {
+			this.selectedVariant = index
 		}
 	},
 	computed: {
+		image() {
+			return this.variants[this.selectedVariant].variantImg
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity
+		},
+		onSale() {
+			return this.variants[this.selectedVariant].variantQuantity > 10
+		},
 		title() {
-			return this.brand + ' ' + this.product
+			let sale = this.onSale ? ' (sale)' : '';
+			return this.brand + ' ' + this.product + sale
 		}
 	}
 });
