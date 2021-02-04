b7905c25677db7f106b02642b8d07d9c88ca3773
Timur <t.fralik@aimprosoft.com>
2021-02-04 17:12:11 +0200

    Add routers examples

--- 02-real-world-vue/src/router/index.js
+++ 02-real-world-vue/src/router/index.js
@@ -11,13 +11,18 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "About",
+    alias: "/about-old",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/about",
+    redirect: { name: "About" }
   }
 ];
 
